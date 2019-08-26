53e8e9c5c6f77d67a9e7e04be1a4bb159471b3bb
Allan Amaral <allanmaral@hotmail.com>
2019-08-26 18:44:35 -0300

    Updates

---
+++ precache-manifest.5988c39fddc094d9ce11d62f28fae315.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edbbc12f1b1b0f711fc8ed65a7e7af48",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "62712c027971fd36ac59",
+    "url": "/monster-rolodex/static/css/main.a23f9f6e.chunk.css"
+  },
+  {
+    "revision": "e6f182edb81f551cb0c1",
+    "url": "/monster-rolodex/static/js/2.a4cf3094.chunk.js"
+  },
+  {
+    "revision": "62712c027971fd36ac59",
+    "url": "/monster-rolodex/static/js/main.8a813282.chunk.js"
+  },
+  {
+    "revision": "1f1fdb189afd82863836",
+    "url": "/monster-rolodex/static/js/runtime~main.80659f76.js"
+  }
+]);
